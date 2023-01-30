d63798c36b02ada9a3395a8266be52ba782c81fe
Hiroaki06 <123339108+Hiroaki06@users.noreply.github.com>
2023-01-30 10:12:20 +0900

    main.js作成
    とりあえずあげます。

---
+++ main.js
@@ -0,0 +1,20 @@
+var speedH;  //km/h
+var speedS;  //m/s
+var target;  //目的距離
+var distance;   //距離
+var seconds;    //秒
+
+document.querySelector(".jnknlist").addEventListener("click",function(e){
+        countUP();
+    
+    document.querySelector(".jnknlist").addEventListener("click",function(e){
+        conversion(speedH);
+    });
+});
+
+function countUP(){ //時間計測
+    
+}
+function conversion(Hspeed){    //速度変換
+    speedS = (Hspeed * 1000) / 3600;
+}

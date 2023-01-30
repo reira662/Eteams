f6accad428070be71cabae2c10d88cdea7a450c6
Hiroaki06 <123339108+Hiroaki06@users.noreply.github.com>
2023-01-30 12:00:46 +0900

    html・js追加
    ページ遷移
    ※エラーあり

--- main.js
+++ main.js
@@ -1,21 +1,28 @@
-var speedH;  //km/h
-var speedS;  //m/s
-var target;  //目的距離
-var distance;   //距離
-var seconds;    //秒
+let timer;
+let time = 0;
+let speedH;  //km/h
+let speedS;  //m/s
+let target;  //目的距離
+let distance;   //距離
+let seconds;    //秒
 
-var jisoku = document.getElementById("jisoku");
-console.log(jisoku.value);
-document.querySelector(".jnknlist").addEventListener("click",function(e){
-        countUP();
+var button = document.getElementById('start');
+// let jisoku = document.getElementById("jisoku");
+// console.log(jisoku.value);
+button.addEventListener("click", function(){
+    window.location.href = "index01.html";
+    console.log("hai");
+    // countUP();
     
-    document.querySelector(".jnknlist").addEventListener("click",function(e){
-        conversion(speedH);
-    });
+    // document.querySelector(".jnknlist").addEventListener("click",function(e){
+        // conversion(speedH);
+    // });
 });
 
 function countUP(){ //時間計測
-    
+    time +=1;
+    console.log(time);
+    timer = setTimeout("countUP()",1000);
 }
 function conversion(Hspeed){    //速度変換
     speedS = (Hspeed * 1000) / 3600;

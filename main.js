37cf2acb02fc04bd13894ebcb062c48294fa5e63
y22506 <y22506@iit.iwate-it.ac.jp>
2023-02-06 10:05:02 +0900

    jsカウント機能
    カウント機能追加
    エラー削除

--- main.js
+++ main.js
@@ -1,4 +1,4 @@
-let timer;
+let timer =10;
 let time = 0;
 let speedH;  //km/h
 let speedS;  //m/s
@@ -9,10 +9,17 @@ let seconds;    //秒
 var button = document.getElementById('start');
 // let jisoku = document.getElementById("jisoku");
 // console.log(jisoku.value);
-button.addEventListener("click", function(){
-    window.location.href = "index01.html";
-    console.log("hai");
-    // countUP();
+window.addEventListener("load", function(){
+    // console.log("hai");
+    // window.location.href:URLを取得
+    // split('/'):'/'区切りの配列
+    // pop():配列の最終要素のみを取得
+    time = 0;
+    var str = window.location.href.split('/').pop();//ファイル名の取得
+    if(str == "index01.html")
+        countUP();
+    // location.href = "index01.html";
+    
     
     // document.querySelector(".jnknlist").addEventListener("click",function(e){
         // conversion(speedH);
@@ -20,9 +27,13 @@ button.addEventListener("click", function(){
 });
 
 function countUP(){ //時間計測
-    time +=1;
-    console.log(time);
-    timer = setTimeout("countUP()",1000);
+    setInterval(() => {
+        time +=1;
+        console.log(time);
+        if(timer <= time)
+        location.href = "index02.html";
+    }, 1000);
+    // timer = setTimeout("countUP()",1000);
 }
 function conversion(Hspeed){    //速度変換
     speedS = (Hspeed * 1000) / 3600;

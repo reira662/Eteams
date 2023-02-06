dacbaabc4f88a80afad0d2a1a561b0f9a40dfe1a
Hiroaki06 <123339108+Hiroaki06@users.noreply.github.com>
2023-02-06 16:28:50 +0900

    js追加
    基本的な操作は完成。
    目的時間等は今後決める。
    最後のページに移動したときに変数の値が出ない。

--- main.js
+++ main.js
@@ -1,25 +1,39 @@
-timer = 20;
-let time = 0;
+var timer = 20;
+var difference;
+var time;
 let speedH;  //km/h
 let speedS;  //m/s
-let target = 1000;  //目的距離[m]
+let target = 10;  //目的距離[m]
 let distance = 0;   //距離[m]
 let seconds;    //秒
 
+//ページを読み込んだ時実行
 window.addEventListener("load", function(){
     // window.location.href:URLを取得
     // split('/'):'/'区切りの配列
     // pop():配列の最終要素のみを取得  
     var str = window.location.href.split('/').pop();//ファイル名の取得
-    time = 0;
+    // if(str == "index.html"){
+    //     document.getElementById('start').addEventListener("click", function(){
+
+    //     });
+    // }
     if(str == "index01.html"){
-        speedH = document.getElementById("jisoku");
+        time = 0;
+        speedH = document.getElementById("jisoku");//時速のDOM
         countUP();
         distanceUP();  
     }
+    if(str == "index02.html"){
+        console.log(time);
+        console.log(timer);
+
+        document.getElementsByTagName("p")[0].innerHTML = "目的の時間と"+ difference +"秒の差がありました。"
+    }
 });
 
-function countUP(){ //時間計測
+//時間計測
+function countUP(){
     setInterval(() => {
         time +=1;
         conversion(speedH.value);
@@ -29,14 +43,20 @@ function countUP(){ //時間計測
     }, 1000);
     // timer = setTimeout("countUP()",1000);
 }
-function conversion(Hspeed){    //速度変換
+
+//速度変換
+function conversion(Hspeed){    
     speedS = (Hspeed * 1000) / 3600;
 }
-function distanceUP(){//距離測定
+
+//距離測定
+function distanceUP(){
     setInterval(() => {
         distance += speedS;
         console.log(distance);
-        if(target <= distance)//距離に達したら移動
-            location.href = "index02.html";    
+        if(target <= distance){//距離に達したら移動
+            difference = time-timer;
+            location.href = "index02.html";//?difference="+difference;
+        }   
     }, 1000);
 }

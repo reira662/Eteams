3bedba7d03f81e3a9dab9bf761282432b424cc33
Hiroaki06 <123339108+Hiroaki06@users.noreply.github.com>
2023-02-06 11:30:06 +0900

    js追加
    時速の読み取り
    距離の変換

--- main.js
+++ main.js
@@ -1,40 +1,42 @@
-let timer =10;
+timer = 20;
 let time = 0;
 let speedH;  //km/h
 let speedS;  //m/s
-let target;  //目的距離
-let distance;   //距離
+let target = 1000;  //目的距離[m]
+let distance = 0;   //距離[m]
 let seconds;    //秒
 
-var button = document.getElementById('start');
-// let jisoku = document.getElementById("jisoku");
-// console.log(jisoku.value);
 window.addEventListener("load", function(){
-    // console.log("hai");
     // window.location.href:URLを取得
     // split('/'):'/'区切りの配列
-    // pop():配列の最終要素のみを取得
-    time = 0;
+    // pop():配列の最終要素のみを取得  
     var str = window.location.href.split('/').pop();//ファイル名の取得
-    if(str == "index01.html")
+    time = 0;
+    if(str == "index01.html"){
+        speedH = document.getElementById("jisoku");
         countUP();
-    // location.href = "index01.html";
-    
-    
-    // document.querySelector(".jnknlist").addEventListener("click",function(e){
-        // conversion(speedH);
-    // });
+        distanceUP();  
+    }
 });
 
 function countUP(){ //時間計測
     setInterval(() => {
         time +=1;
+        conversion(speedH.value);
+        // console.log(speedS);
+        // console.log(speedH.value);
         console.log(time);
-        if(timer <= time)
-        location.href = "index02.html";
     }, 1000);
     // timer = setTimeout("countUP()",1000);
 }
 function conversion(Hspeed){    //速度変換
     speedS = (Hspeed * 1000) / 3600;
+}
+function distanceUP(){//距離測定
+    setInterval(() => {
+        distance += speedS;
+        console.log(distance);
+        if(target <= distance)//距離に達したら移動
+            location.href = "index02.html";    
+    }, 1000);
 }

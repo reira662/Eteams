4005ca4b3ec0fa7ce47e1b57e01d79c977a74bd9
Hiroaki06 <123339108+Hiroaki06@users.noreply.github.com>
2023-02-07 14:06:38 +0900

    難易度追加
    難易度を変えれるようにした。仮

--- main.js
+++ main.js
@@ -1,11 +1,14 @@
-var timer = 20;
+var timer;
 var difference;
 var time;
 let speedH;  //km/h
 let speedS;  //m/s
-let target = 10;  //目的距離[m]
+let target;  //目的距離[m]
 let distance = 0;   //距離[m]
 let seconds;    //秒
+
+let timers = [20,40,60];
+var targets = [100,200,300];
 
 //ページを読み込んだ時実行
 window.addEventListener("load", function(){
@@ -13,21 +16,23 @@ window.addEventListener("load", function(){
     // split('/'):'/'区切りの配列
     // pop():配列の最終要素のみを取得  
     var str = window.location.href.split('/').pop();//ファイル名の取得
-    // if(str == "index.html"){
-    //     document.getElementById('start').addEventListener("click", function(){
-
-    //     });
-    // }
+    if(str == "index.html"){
+        var level = document.getElementById('bottom').addEventListener("click", function(){
+            localStorage.timer = timers[level.value];
+            localStorage.target = targets[level.value]
+        });
+    }
     if(str == "index01.html"){
         time = 0;
+        timer = localStorage.timer;
+        target = localStorage.target;
         speedH = document.getElementById("jisoku");//時速のDOM
         countUP();
         distanceUP();  
     }
     if(str == "index02.html"){
         console.log(time);
-        console.log(timer);
-        var difference = localStorage.difference;
+        difference = localStorage.difference;
         document.getElementsByTagName("p")[0].innerHTML = "目的の時間と"+ difference +"秒の差がありました。";
     }
 });
@@ -39,7 +44,7 @@ function countUP(){
         conversion(speedH.value);
         // console.log(speedS);
         // console.log(speedH.value);
-        console.log(time);
+        //console.log(time);
     }, 1000);
     // timer = setTimeout("countUP()",1000);
 }
@@ -56,7 +61,7 @@ function distanceUP(){
         console.log(distance);
         if(target <= distance){//距離に達したら移動
             localStorage.difference = time-timer;
-            location.href = "index02.html";//?difference="+difference;
+            location.href = "index02.html";
         }   
     }, 1000);
 }
